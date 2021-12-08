cc7c8fb8440c31f86545a91832a5f7e88a7a69ba
JAMorello <juan.ag.morello@gmail.com>
2021-12-08 11:29:46 -0300

    Update root app component, module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { RecursosComponent } from './pages/recursos/recursos.component';
+import { RolesComponent } from './pages/roles/roles.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'roles', component: RolesComponent },
+  { path: 'recursos', component: RecursosComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
+// pages
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { RolesComponent } from './pages/roles/roles.component';
+import { RecursosComponent } from './pages/recursos/recursos.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+// components
+import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    RolesComponent,
+    RecursosComponent,
+    InicioComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
