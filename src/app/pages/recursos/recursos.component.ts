c08ba37b987963e965bd32556766840d023fa484
JAMorello <juan.ag.morello@gmail.com>
2021-12-09 01:55:45 -0300

    Implement recursos.service and create list of cards on where display data

--- src/app/pages/recursos/recursos.component.ts
+++ src/app/pages/recursos/recursos.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RecursosService } from 'src/app/services/recursos.service';
+import { Recurso } from 'src/app/model/recurso';
 @Component({
   selector: 'app-recursos',
   templateUrl: './recursos.component.html',
-  styleUrls: ['./recursos.component.css']
+  styleUrls: ['./recursos.component.css'],
 })
 export class RecursosComponent implements OnInit {
+  recursos: Recurso[] = [];
 
-  constructor() { }
+  constructor(private _recursosService: RecursosService) {}
 
   ngOnInit(): void {
+    this.cargarRecursos();
   }
 
+  cargarRecursos() {
+    this._recursosService
+      .getRecursos()
+      .subscribe((data) => (this.recursos = data));
+  }
 }
