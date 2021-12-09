7e360e4d73838ea43f7917e33790892bed4ce249
JAMorello <juan.ag.morello@gmail.com>
2021-12-09 01:55:29 -0300

    Implement roles.service and create list of cards on where display data

--- src/app/pages/roles/roles.component.ts
+++ src/app/pages/roles/roles.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RolesService } from 'src/app/services/roles.service';
+import { Rol } from 'src/app/model/rol';
 @Component({
   selector: 'app-roles',
   templateUrl: './roles.component.html',
-  styleUrls: ['./roles.component.css']
+  styleUrls: ['./roles.component.css'],
 })
 export class RolesComponent implements OnInit {
+  roles: Rol[] = [];
 
-  constructor() { }
+  constructor(private _rolesService: RolesService) {}
 
   ngOnInit(): void {
+    this.cargarRoles();
   }
 
+  cargarRoles() {
+    this._rolesService.getRoles().subscribe((data) => (this.roles = data));
+  }
 }
