d1c64f2dfe5bf516606de421aff9cd6119539c3c
JAMorello <juan.ag.morello@gmail.com>
2021-12-09 01:55:01 -0300

    Implement usuarios.service and create table on where display data

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import { Usuario } from 'src/app/model/usuario';
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.css']
+  styleUrls: ['./usuarios.component.css'],
 })
 export class UsuariosComponent implements OnInit {
+  usuarios: Usuario[] = [];
+  displayedColumns: string[] = [
+    'Nombre',
+    'Apellido',
+    'Username',
+    'Password',
+    'Email',
+    'Estado',
+  ];
 
-  constructor() { }
+  constructor(private _usuariosService: UsuariosService) {}
 
   ngOnInit(): void {
+    this.cargarUsuarios();
   }
 
+  cargarUsuarios() {
+    this._usuariosService
+      .getUsuarios()
+      .subscribe((data) => (this.usuarios = data));
+  }
 }
