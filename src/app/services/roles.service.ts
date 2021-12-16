a5b021dfa1b9d1825e89fa2975fb8a8424242ed1
JAMorello <juan.ag.morello@gmail.com>
2021-12-15 23:34:47 -0300

    Implement various http requests for api/Rol

--- src/app/services/roles.service.ts
+++ src/app/services/roles.service.ts
@@ -1,15 +1,54 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { JwtService } from './jwt.service';
 import { Rol } from '../model/rol';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RolesService {
-  constructor(private http: HttpClient) {}
+  baseUrl: string = '';
 
-  getRoles(): Observable<Rol[]> {
-    return this.http.get<Rol[]>('./assets/data/roles.json');
+  constructor(private http: HttpClient, private jwtService: JwtService) {
+    this.baseUrl = environment.baseUrl + 'Rol';
+  }
+
+  generateHeaders() {
+    return {
+      headers: new HttpHeaders().set('token', this.jwtService.getJwt()),
+    };
+  }
+
+  getRoles() {
+    return this.http.get<Rol[]>(this.baseUrl, this.generateHeaders());
+  }
+
+  getRol(id: number) {
+    return this.http.get<Rol>(`${this.baseUrl}/${id}`, this.generateHeaders());
+  }
+
+  createRol(rol: Rol) {
+    return this.http.post<Rol>(
+      `${this.baseUrl}/Create`,
+      rol,
+      this.generateHeaders()
+    );
+  }
+
+  updateRol(rol: Rol) {
+    return this.http.put<Rol>(
+      `${this.baseUrl}/Update/${rol.idRol}`,
+      rol,
+      this.generateHeaders()
+    );
+  }
+
+  deleteRol(id: number) {
+    return this.http.delete<Rol>(
+      `${this.baseUrl}/Delete/${id}`,
+      this.generateHeaders()
+    );
   }
 }
