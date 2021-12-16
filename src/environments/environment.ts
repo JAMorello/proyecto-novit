7a0f5c592cfb741b9ad0a395625174249c7695c8
JAMorello <juan.ag.morello@gmail.com>
2021-12-15 23:33:26 -0300

    Add baseUrl and token for Http requests

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,18 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'http://localhost:5000/api/',
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY3RpdmlkYWRJbnRlZ3JhZG9yYSIsIm5hbWUiOiJub3ZpdCIsImlhdCI6MTcxNjIzOTAyMn0.ZmiSFwikRE7FZHa_bhZpFN6mYdtWSNTJbueCFMt_c1M',
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+ActividadIntegradoraAPI
+appsettings.json
+----------------
+  "Jwt": {
+    "secret": "Password1234567890!",
+    "sub": "actividadIntegradora",
+    "name": "novit"
+  }
+
+*/
